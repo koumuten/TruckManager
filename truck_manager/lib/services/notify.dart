8ee47b898d57b6973b21f7d7fd8a4f4ac17fc9ed
k0umutensagy0ba <k0umutensagy0ba@gmail.com>
2026-04-22 01:28:51 +0000

    Add Gas Notify Service

---
+++ truck_manager/lib/services/notify.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+import 'package:truck_manager/services/asset_loader.dart';
+
+class GASNotifyService{
+  static Future<void> notifyErrorToGas(String message) async {
+    try {
+      // GASのデプロイURL (GETパラメータとしてメッセージを送信)
+      final url = Uri.parse(await AssetLoader.readAsset("GAS_WEB_HOOK"))
+          .replace(queryParameters: {'message': message});
+      
+      await http.get(url);
+    } catch (e) {
+      print("GASへの通知自体に失敗しました: $e");
+    }
+  }
+}
