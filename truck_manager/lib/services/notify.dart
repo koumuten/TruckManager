f0ba51a08bee723bc3459d154646e639e590200b
k0umutensagy0ba <k0umutensagy0ba@gmail.com>
2026-04-22 03:18:41 +0000

    print debug message

--- truck_manager/lib/services/notify.dart
+++ truck_manager/lib/services/notify.dart
@@ -1,13 +1,14 @@
 import 'package:http/http.dart' as http;
 import 'package:truck_manager/services/asset_loader.dart';
 
-class GASNotifyService{
+class GASNotifyService {
   static Future<void> notifyErrorToGas(String message) async {
+    print(message);
     try {
       // GASのデプロイURL (GETパラメータとしてメッセージを送信)
       final url = Uri.parse(await AssetLoader.readAsset("GAS_WEB_HOOK"))
           .replace(queryParameters: {'message': message});
-      
+
       await http.get(url);
     } catch (e) {
       print("GASへの通知自体に失敗しました: $e");
